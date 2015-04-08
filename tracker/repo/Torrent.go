11b462bf028e169382302a578212bcc5fdb04a27
Julien Bonachera <bonachera.julien@me.com>
2015-04-09 00:45:55 +0200

    compute leechers, seeders and completed counters in repo files

--- tracker/repo/Torrent.go
+++ tracker/repo/Torrent.go
@@ -9,7 +9,7 @@ import (
 // the file's hash. The completed counter is set to 0, as the file was just
 // created.
 func NewTorrent(c models.ApplicationContext, hash string) models.Torrent {
-	return models.Torrent{hash, NewPeerListFromHash(c, hash), 0}
+	return models.Torrent{hash, NewPeerListFromHash(c, hash), 0, 0, 0}
 }
 
 func GetTorrent(c models.ApplicationContext, hash string) models.Torrent {
@@ -22,6 +22,14 @@ func GetTorrent(c models.ApplicationContext, hash string) models.Torrent {
 	} else {
 		t.Peers = NewPeerListFromHash(c, hash)
 	}
+	complete, incomplete := 0, len(t.Peers)
+	for _, peer := range t.Peers {
+		if peer.Left == 0 {
+			complete += 1
+			incomplete -= 1
+		}
+	}
+
 	return t
 }
 
